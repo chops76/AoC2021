23de9c5bfc872a22378805fc619c6862b91ddddb
Dan Chopskie <dchopskie@gmail.com>
2021-12-14 07:10:36 -0600

    or_insert is the best

--- src/day14.rs
+++ src/day14.rs
@@ -39,13 +39,9 @@ fn part1(rules: &HashMap<String, String>, template: &str) -> usize {
         work_str = apply_rules(rules, &work_str);
     }
 
-    let mut counts = HashMap::new();
+    let mut counts:HashMap<char, usize> = HashMap::new();
     for c in work_str.chars() {
-        if !counts.contains_key(&c) {
-            counts.insert(c, 1);
-        } else {
-            *counts.get_mut(&c).unwrap() += 1;
-        }
+        *counts.entry(c).or_insert(0) += 1;
     }
     *counts.iter().map(|(_, c)| c).max().unwrap() - *counts.iter().map(|(_, c)| c).min().unwrap()
 }
@@ -54,35 +50,19 @@ fn part2(rules: &HashMap<String, String>, template: &str) -> usize {
 
     let mut counts = HashMap::new();
     for i in 0..template.len() - 1 {
-        if !counts.contains_key(&template[i..i+2]) {
-            counts.insert(template[i..i+2].to_string(), 1);
-        } else {
-            *counts.get_mut(&template[i..i+2]).unwrap() += 1;
-        }
+        *counts.entry(template[i..i+2].to_string()).or_insert(0) += 1;
     }
     
     for _ in 0..40 {
-        let mut new_counts = HashMap::new();
+        let mut new_counts:HashMap<String, usize> = HashMap::new();
         for (item_key, item_count) in &counts {
             if rules.contains_key(item_key) {
                 let first_str = item_key[0..1].to_string() + &rules[item_key];
                 let second_str = rules[item_key].to_string() + &item_key[1..];
-                if !new_counts.contains_key(&first_str) {
-                    new_counts.insert(first_str, *item_count);
-                } else {
-                    *new_counts.get_mut(&first_str).unwrap() += item_count;
-                }
-                if !new_counts.contains_key(&second_str) {
-                    new_counts.insert(second_str, *item_count);
-                } else {
-                    *new_counts.get_mut(&second_str).unwrap() += item_count;
-                }
+                *new_counts.entry(first_str).or_insert(0) += item_count;
+                *new_counts.entry(second_str).or_insert(0) += item_count;
             } else {
-                if !new_counts.contains_key(item_key) {
-                    new_counts.insert(item_key.to_string(), *item_count);
-                } else {
-                    *new_counts.get_mut(item_key).unwrap() += item_count;
-                }                
+                *new_counts.entry(item_key.to_string()).or_insert(0) += item_count;
             }
         }
         counts = new_counts;
@@ -92,14 +72,11 @@ fn part2(rules: &HashMap<String, String>, template: &str) -> usize {
     for (item_key, item_count) in &counts {
         let chars = item_key.chars().collect::<Vec<char>>();
         let c = chars[1];
-        if !char_counts.contains_key(&c) {
-            char_counts.insert(c, *item_count);
-        } else {
-            *char_counts.get_mut(&c).unwrap() += item_count;
-        }
+        *char_counts.entry(c).or_insert(0) += item_count;
     }
     let first_char = template.chars().next().unwrap();
     *char_counts.get_mut(&first_char).unwrap() += 1;
+
     *char_counts.iter().map(|(_, c)| c).max().unwrap() - *char_counts.iter().map(|(_, c)| c).min().unwrap()
 }
 
